a47aa667172fb9c84d64465077f005e249b86a6d
diaz flores <joandiazflores06@gmail.com>
2025-04-25 08:36:52 -0500

    proyecto

---
+++ carrucel.js
@@ -0,0 +1,8 @@
+<div class="carousel">
+<button class="prev" onclick="moveCarousel(-1)">&#10094;</button>
+<div class="carousel-track">
+    <div class="carousel-item">
+
+<button class="next" onclick="moveCarousel(1)">&#10095;</button>
+</div>
+</section>
